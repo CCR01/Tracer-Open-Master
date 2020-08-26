b244c2bdbf2589b27e50ac074fc08d85d8ef960c
CCR01 <30525471+CCR01@users.noreply.github.com>
2020-08-26 17:10:30 +0200

    tAll for server

--- LowLevelTracing/main_LLT.cpp
+++ LowLevelTracing/main_LLT.cpp
@@ -143,7 +143,7 @@ int main(int argc, char **argv)
 	// ManagementTests managementTest_2(testWhatVec_2);
 	// workTheSystem.push_back(managementTest_2.testSuperFct());
 
-	std::vector<testWhat> testWhatVec_3 = { testWhat::tCardinalPoints };
+	std::vector<testWhat> testWhatVec_3 = { testWhat::tALL };
 	ManagementTests managementTest_3(testWhatVec_3);
 	workTheSystem.push_back(managementTest_3.testSuperFct());
 
